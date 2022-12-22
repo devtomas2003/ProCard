1b4fbdeaae0b431367dc3bd41b2f363923c6deb2
Tomás Dinis <dev.tomas2003@gmail.com>
2022-12-22 23:24:10 +0000

    Source links correction

--- ProCard/src/Views/Refeitorio/Login/index.tsx
+++ ProCard/src/Views/Refeitorio/Login/index.tsx
@@ -61,7 +61,7 @@ export default function Login(){
                             <BtnText>Entrar</BtnText>
                         </BtnLogin>
                     </BtnsAction>
-                    <LineCopyright>2022 - {new Date().getFullYear()} © <LinkProject>ProCard</LinkProject>. <LinkProject>MIT License</LinkProject></LineCopyright>
+                    <LineCopyright>2022 - {new Date().getFullYear()} © <LinkProject onClick={() => { alert("https://github.com/devtomas2003/ProCard"); }}>ProCard</LinkProject>. <LinkProject onClick={() => { alert("https://github.com/devtomas2003/ProCard/blob/Development/LICENSE"); }}>MIT License</LinkProject></LineCopyright>
                 </LoginContainer>
             </LoginBox>
         </Container>
